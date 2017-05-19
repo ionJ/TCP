a73a6f0a79083e5677f603b012b5e6f449b1a2ea
ionJ <ion_J@outlook.com>
2017-05-19 23:08:57 +0800

    新增了个滑动菜单按钮
    将默认的导航按钮替换为滑动菜单按钮，按下后可以很方便地打开滑动菜单。

--- app/src/main/java/com/example/android/scp/MainActivity.java
+++ app/src/main/java/com/example/android/scp/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.android.scp;
 
+
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +20,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        // 通过 setDisplayHomeAsUpEnabled() 让导航按钮显示出来
+        // 调用 setHomeAsUpIndicator() 来设置一个导航图标
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            default:
+        }
+        return true;
     }
 }
